e84178ee5566b931e45073fca7064e0d80b4563e
Felipe Frazão <felipe.frazao@gmail.com>
2022-05-04 21:44:52 -0300

    iniciando

---
+++ src/main/java/br/com/keytime/cadastroPessoal/servlet/Pessoa.java
@@ -0,0 +1,46 @@
+package br.com.keytime.cadastroPessoal.servlet;
+
+import java.util.Date;
+
+public class Pessoa {
+
+	private String nome;
+	private String CPF;
+	private Date dataNasc;
+	private String endereco;
+	private String telefone;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+}
